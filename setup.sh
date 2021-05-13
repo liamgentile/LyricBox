0c11a4264c57fef7649f588303a64d817da51e2a
Liam Gentile <80293145+liamgentile@users.noreply.github.com>
2021-05-13 11:40:35 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"liam.gentile@mail.mcgill.ca\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml

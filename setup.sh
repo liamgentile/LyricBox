8e0be1f9a644a4253f95edecee7264449fbc62e9
Liam Gentile <80293145+liamgentile@users.noreply.github.com>
2021-05-13 12:09:43 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"liam.gentile@mail.mcgill.ca\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\

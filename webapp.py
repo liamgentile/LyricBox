89731a8068f76babc94e15800842452fee0d0587
Liam Gentile <80293145+liamgentile@users.noreply.github.com>
2021-05-15 19:03:12 -0400

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -41,9 +41,9 @@ genres = st.selectbox("Which genre do you want to stylize your idea generator?",
 
 def s3_get_keras_model(model_name: str) -> tensorflow.keras.Model:
   with tempfile.TemporaryDirectory() as tempdir:
-    s3fs = s3fs.S3FileSystem()
+    s3 = s3fs.S3FileSystem()
     # Fetch and save the zip file to the temporary directory
-    s3fs.get(f"{BUCKET_NAME}/{model_folder}/{model_name}.zip", f"{tempdir}/{model_folder}/{model_name}.zip")
+    s3.get(f"{BUCKET_NAME}/{model_folder}/{model_name}.zip", f"{tempdir}/{model_folder}/{model_name}.zip")
     # Extract the model zip file within the temporary directory
     with zipfile.ZipFile(f"{tempdir}/{model_folder}/{model_name}.zip") as zip_ref:
         zip_ref.extractall(f"{tempdir}/{model_folder}/{model_name}")

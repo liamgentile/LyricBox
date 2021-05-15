eba98fa908cf313b2f7d2c8de62f3027feab5431
Liam Gentile <80293145+liamgentile@users.noreply.github.com>
2021-05-15 18:35:16 -0400

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -40,7 +40,7 @@ def s3_get_keras_model(model_name: str) -> tensorflow.keras.Model:
   with tempfile.TemporaryDirectory() as tempdir:
     s3 = s3fs.S3FileSystem()
     # Fetch and save the zip file to the temporary directory
-    s3.get(f"{lyricbox}/{models}/{model_name}.zip", f"{tempdir}/{model_name}.zip")
+    s3.get('lyricbox/models/{}'.format(model_name).zip, f"{tempdir}/{model_name}.zip")
     # Extract the model zip file within the temporary directory
     with zipfile.ZipFile(f"{tempdir}/{model_name}.zip") as zip_ref:
         zip_ref.extractall(f"{tempdir}/{model_name}")

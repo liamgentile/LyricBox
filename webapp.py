2daa60119a91a50feee3cd47dbb6304546c0df34
Liam Gentile <80293145+liamgentile@users.noreply.github.com>
2021-05-15 13:21:38 -0400

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model
 import s3fs
 import h5py
 import pickle
@@ -35,9 +36,9 @@ genres = st.selectbox("Which genre do you want to stylize your idea generator?",
 
 s3 = s3fs.S3FileSystem()
 # importing models from s3 bucket
-folk_model = h5py.File(s3.open("s3://lyricbox/models/folk_lyrics_RNN_model4.h5", "rb"))
-folk_model = h5py.File(s3.open("s3://lyricbox/models/pop_lyric_model.h5", "rb"))
-folk_model = h5py.File(s3.open("s3://lyricbox/models/rap_lyric_model.h5", "rb"))
+folk_model = load_model(h5py.File(s3.open("s3://lyricbox/models/folk_lyrics_RNN_model4.h5", "rb")))
+folk_model = load_model(h5py.File(s3.open("s3://lyricbox/models/pop_lyric_model.h5", "rb")))
+folk_model = load_model(h5py.File(s3.open("s3://lyricbox/models/rap_lyric_model.h5", "rb")))
 
 
 #tokenizer_folk import

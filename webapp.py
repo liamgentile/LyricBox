718ae8cc0745b4266d14c5cbc48bdffa47f8b925
Liam Gentile <80293145+liamgentile@users.noreply.github.com>
2021-05-16 19:38:56 -0400

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -46,9 +46,8 @@ def get_s3fs():
 
 def s3_get_keras_model(model_name: str) -> tensorflow.keras.Model:
   with tempfile.TemporaryDirectory() as tempdir:
-    s3fs = get_s3fs()
     # Fetch and save the zip file to the temporary directory
-    s3fs.get(f"{BUCKET_NAME}/{model_folder}/{model_name}.zip", f"{tempdir}/{model_folder}/{model_name}.zip")
+    get_s3fs(f"{BUCKET_NAME}/{model_folder}/{model_name}.zip", f"{tempdir}/{model_folder}/{model_name}.zip")
     # Extract the model zip file within the temporary directory
     with zipfile.ZipFile(f"{tempdir}/{model_folder}/{model_name}.zip") as zip_ref:
         zip_ref.extractall(f"{tempdir}/{model_folder}/{model_name}")

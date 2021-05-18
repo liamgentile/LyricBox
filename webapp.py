8fcb96658e74db86cb6876d171f336b367baebda
Liam Gentile <80293145+liamgentile@users.noreply.github.com>
2021-05-18 11:26:07 -0400

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -38,14 +38,11 @@ word_count = st.selectbox("How many words do you want to generate?", word_count_
 genre_options = ['folk', 'pop', 'hip hop']
 genres = st.selectbox("Which genre do you want to stylize your idea generator?", genre_options)
 
+s3 = s3fs.S3FileSystem(anon=True)
 
-folk_model = load_model('s3://lyricbox/models/folk_lyrics_RNN_model4.h5', compile=False) 
-pop_model = load_model('s3://lyricbox/models/pop_lyric_model.h5', compile=False) 
-hiphop_model = load_model('s3://lyricbox/models/rap_lyric_model.h5', compile=False) 
-
-
-	
-
+folk_model = load_model(s3.open('s3://lyricbox/models/folk_lyrics_RNN_model4.h5', compile=False), 'rb') 
+pop_model = load_model(s3.open('s3://lyricbox/models/pop_lyric_model.h5', compile=False), 'rb')
+hiphop_model = load_model(s3.open('s3://lyricbox/models/rap_lyric_model.h5', compile=False), 'rb')
 
 #tokenizer_folk import
 tokenizer_folk = pickle.load(s3.open('s3://lyricbox/tokenizers/folk_tokenizer.pkl','rb'))

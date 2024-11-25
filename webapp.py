5509b86f6e41b2db8ecba5ce0858804ad64a6d3d
Liam Gentile <liamanthonygentile@gmail.com>
2024-11-25 13:57:56 -0800

    remove unused imports

--- webapp.py
+++ webapp.py
@@ -1,8 +1,6 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.models import Sequential
 from tensorflow.keras.models import load_model
 from tensorflow.python.keras.utils.data_utils import get_file
 import pickle
